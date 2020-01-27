16b79c7b1a4d55bb4d37109f35fb28c5bb385b4a
bdalleva <bdalleva422@gmail.com>
2020-01-27 12:18:20 -0500

    Register

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+model: any = {};
+  constructor(private authservice: AuthService) { }
+
+  ngOnInit() {
+  }
+login() {
+  this.authservice.login(this.model).subscribe(next => {
+    console.log('Log in success')
+  }, error =>{console.log('Log in Fail')});
+}
+
+loggedIn(){
+  const token = localStorage.getItem('token');
+  return !!token;
+}
+logout(){
+  localStorage.removeItem('token');
+  console.log('logged out');
+}
+}
--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,19 +20,20 @@ namespace DatingApp.API.Controllers
             _context = context;
 
         }
-       [AllowAnonymous]
+        [AllowAnonymous]
         [HttpGet]
-        public async Task<IActionResult>  GetValues()
+        public async Task<IActionResult> GetValues()
         {
             var values = await _context.Values.ToListAsync();
             return Ok(values);
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
-            var value = await _context.Values.FirstOrDefaultAsync(x=>x.Id==id);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
             return Ok(value);
         }
 
