8775368fb204292972de29e3df3045f280e1d2f9
bdalleva <bdalleva422@gmail.com>
2020-02-03 14:34:08 -0500

    Routes added

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,14 +10,18 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(public authservice: AuthService, private alertify: AlertifyService) { }
+  constructor(public authservice: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
   login() {
     this.authservice.login(this.model).subscribe(next => {
       this.alertify.success('Log in success');
-    }, error => { this.alertify.error(error); });
+    }, error => {
+      this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
+    });
   }
 
   loggedIn() {
@@ -24,6 +29,7 @@ export class NavComponent implements OnInit {
   }
   logout() {
     localStorage.removeItem('token');
-    this.alertify.message('logged out');
+    this.alertify.success('logged out');
+    this.router.navigate(['/home']);
   }
 }
